e686e86049e4f1767ae9d2b97bd3c0f072fb5ba5
Sabbirahmed404 <msa29.cse@gmail.com>
2025-03-11 11:19:09 +0600

    added preview image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,13 @@ export default function Document() {
           href='https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap'
           rel='stylesheet'
         />
+        {/* Open Graph / Social Media Meta Tags */}
+        <meta property="og:title" content="Green University Photography Club" />
+        <meta property="og:description" content="Official website of Green University Photography Club (GUPC)" />
+        <meta property="og:image" content="/assets/meta/link.png" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="/assets/meta/link.png" />
         <style dangerouslySetInnerHTML={{ __html: `
           /* Debug styles to help identify layout issues */
           /* Uncomment the line below to see element boundaries */
